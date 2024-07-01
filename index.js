1af44f0dd2700bb7bb9e353f81d2e847d0a0bb04
Gaurav Kumar Thakur <gaurav.21scse1011042@galgotiasuniversity.edu.in>
2024-07-01 23:22:19 +0530

    updated Static Homepage

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
 //Using Express Static Middlewares to load static files
-//app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 // Setting middleware Engines
 app.set('views', path.join(__dirname, './views'));
